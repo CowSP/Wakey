7c77ae4856c7631e6b42a0df9fed6a193a90b4fd
Justin Liu <44413809+JustACowSP@users.noreply.github.com>
2024-01-01 10:29:10 -0500

    Create app ViewModel with basic functionality

---
+++ app/src/main/java/com/example/wakey/ui/WakeyViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.wakey.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.wakey.WakeyApplication
+import com.example.wakey.data.Alarm
+import com.example.wakey.data.Pattern
+import com.example.wakey.data.WakeyRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+data class AlarmListUiState(
+    val alarms: List<Alarm> = listOf(),
+    val patternsById: Map<Int, Pattern> = mapOf()
+)
+
+class WakeyViewModel(
+    private val wakeyRepository: WakeyRepository
+) : ViewModel() {
+    private val _alarmListUiState = MutableStateFlow(AlarmListUiState())
+    val alarmListUiState: StateFlow<AlarmListUiState> = _alarmListUiState.asStateFlow()
+
+    init {
+        getAlarmListData()
+    }
+
+    fun getAlarmListData() {
+        viewModelScope.launch {
+            val alarms = wakeyRepository.getAllAlarms().first()
+            val map = buildMap<Int, Pattern> {
+                alarms.forEach { alarm ->
+                    val pattern = wakeyRepository.getPatternById(alarm.patternId).first()
+                    put(alarm.patternId, pattern)
+                }
+            }
+
+            _alarmListUiState.update {
+                it.copy(
+                    alarms = alarms,
+                    patternsById = map
+                )
+            }
+        }
+    }
+
+    companion object {
+        val Factory = viewModelFactory {
+            initializer {
+                val application = this[APPLICATION_KEY] as WakeyApplication
+                WakeyViewModel(application.container.wakeyRepository)
+            }
+        }
+    }
+}
